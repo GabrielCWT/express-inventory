0c28260c4522f6418c6c6935c095b094157a1be1
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-07-21 11:11:06 +0800

    Implement individual category page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,33 @@
 const Category = require('../models/category');
+const Item = require('../models/item');
 
 const asyncHandler = require('express-async-handler');
 
 exports.category_list = asyncHandler(async (req, res, next) => {
   const allCategories = await Category.find({}, 'name').exec();
 
-  res.render('category_list', {
+  res.render('list_layout', {
     title: 'Category List',
-    category_list: allCategories,
+    header: 'Categories',
+    list_arr: allCategories,
+  });
+});
+
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const [currentCategory, allItemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).exec(),
+  ]);
+
+  if (currentCategory === null) {
+    const err = new Error('No such category');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('list_layout', {
+    title: currentCategory.name,
+    category: currentCategory,
+    list_arr: allItemsInCategory,
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,15 @@ router.get('/', function (req, res, next) {
 
 /* Catalog routing */
 router.get('/categories', categoryController.category_list);
-// TODO router.get('/categories/:id', categoryController.category_detail);
-// TODO router.get('/categories/:id/create', categoryController.category_create);
+router.get('/categories/:id', categoryController.category_detail);
+// TODO router.get('/categories/create', categoryController.category_create);
 // TODO router.get('/categories/:id/update', categoryController.category_update);
 // TODO router.get('/categories/:id/delete', categoryController.category_delete);
 
 /* Items routing */
 router.get('/items', itemController.item_list);
 // TODO router.get('/items/:id', itemController.item_detail);
-// TODO router.get('/items/:id/create', itemController.item_create);
+// TODO router.get('/items/create', itemController.item_create);
 // TODO router.get('/items/:id/update', itemController.item_update);
 // TODO router.get('/items/:id/delete', itemController.item_delete);
 
